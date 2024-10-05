d7278b42c46182ef2af704b950f81edc27a727d9
njoyshadow <shs3740413@gmail.com>
2024-10-06 01:59:13 +0900

    feat: update AttendanceDbContext crud

--- src/main/java/com/isekai/attendancecheck/database/AttendanceDbContext.java
+++ src/main/java/com/isekai/attendancecheck/database/AttendanceDbContext.java
@@ -7,21 +7,17 @@ import com.j256.ormlite.support.ConnectionSource;
 
 import net.minecraft.server.network.ServerPlayerEntity;
 
-
 import java.sql.SQLException;
-import java.util.Date;
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
 
 import static com.isekai.attendancecheck.Attendancecheck.LOGGER;
 
 public class AttendanceDbContext {
     private final ConnectionSource connectionSource;
     private final Dao<UserAttendance, String> userAttendanceDao;
-    private final Dao<UserMonthlyAttendance,Integer> userMonthlyAttendanceDao;
+    private final Dao<UserMonthlyAttendance, Integer> userMonthlyAttendanceDao;
 
-    public AttendanceDbContext(ConnectionSource connectionSource){
+    public AttendanceDbContext(ConnectionSource connectionSource) {
         this.connectionSource = connectionSource;
         try {
             userAttendanceDao = DaoManager.createDao(connectionSource, UserAttendance.class);
@@ -31,24 +27,22 @@ public class AttendanceDbContext {
         }
     }
 
-    public boolean createUser(ServerPlayerEntity player){
+    public boolean createUser(ServerPlayerEntity player) {
         return createUser(player.getUuid(), player.getName().getString());
     }
 
     public boolean createUser(UUID playerUUID, String playerName) {
-        //유저가 이미 있는지 확인
+        // 유저가 이미 있는지 확인
         var user = fetchUser(playerUUID);
-        if(user.isPresent()){
+        if (user.isPresent()) {
             LOGGER.info("User already exists", playerName);
             return false;
         }
 
-        //유저 생성
+        // 유저 생성
         var createDate = new Date();
         var uuidAsString = playerUUID.toString();
         try {
-
-
             var newUser = UserAttendance.builder()
                     .id(playerUUID.toString())
                     .userName(playerName)
@@ -56,11 +50,11 @@ public class AttendanceDbContext {
                     .lastLogin(createDate)
                     .loginCount(1)
                     .loginStack(1)
+                    .totalLoginCount(1)
                     .maxLoginStack(1)
                     .build();
 
             userAttendanceDao.create(newUser);
-
 
             var userMonth = UserMonthlyAttendance.builder()
                     .userAttendance(newUser)
@@ -76,7 +70,77 @@ public class AttendanceDbContext {
         return true;
     }
 
-    public boolean updateUser() {
+    public boolean updateUser(UUID playerUUID, int count) {
+        var _user = fetchUser(playerUUID);
+        if (_user.isEmpty()) {
+            return false;
+        }
+
+        var user = _user.get();
+        var today = new Date();
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(today);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+        var todayMidnight = calendar.getTime();
+        calendar.set(Calendar.DAY_OF_YEAR, -1);
+        var yesterdayMidnight = calendar.getTime();
+
+        //어제 접속 했는가
+        var isYesterdayLogin = user.getLastLogin().after(yesterdayMidnight) && user.getLastLogin().before(todayMidnight);
+        //오늘 접속 했는가?
+        var isTodayLogin = user.getLastLogin().after(todayMidnight);
+        //월 초인가?
+        var isMonthStart = user.getLastLogin().getMonth() != today.getMonth();
+        try {
+            //오늘 접속 안했다면?
+            if(!isTodayLogin){
+                if(isMonthStart){
+                    user.setLoginCount(1);
+                }else {
+                    user.setLoginCount(user.getLoginCount() + count);
+                }
+
+                user.setTotalLoginCount(user.getTotalLoginCount() + count);
+                if(isYesterdayLogin) {
+                    user.setLoginStack(user.getLoginStack() + count);
+                    user.setMaxLoginStack(Math.max(user.getMaxLoginStack(), user.getLoginStack()));
+                } else{
+                    user.setLoginStack(1);
+                }
+            }
+
+            user.setLastLogin(today);
+            userAttendanceDao.update(user);
+
+            // 월별 출석 업데이트
+            var userMonthlyAttendance = userMonthlyAttendanceDao.queryBuilder()
+                    .where()
+                    .eq("userAttendance_id", user.getId())
+                    .and()
+                    .eq("month", today.getMonth())
+                    .queryForFirst();
+
+            if (userMonthlyAttendance == null) {
+                userMonthlyAttendance = UserMonthlyAttendance.builder()
+                        .userAttendance(user)
+                        .month(today.getMonth())
+                        .count(count)
+                        .build();
+                userMonthlyAttendanceDao.create(userMonthlyAttendance);
+            } else {
+                if(!isTodayLogin){
+                    userMonthlyAttendance.setCount(userMonthlyAttendance.getCount() + count);
+                    userMonthlyAttendanceDao.update(userMonthlyAttendance);
+                }
+            }
+        } catch (SQLException e) {
+            LOGGER.error("Failed to update user", e);
+            return false;
+        }
 
         return true;
     }
@@ -87,7 +151,7 @@ public class AttendanceDbContext {
 
     public boolean deleteUser(UUID playerUUID) {
         try {
-            //TODO: delete monthly attendance
+            // TODO: delete monthly attendance
             userAttendanceDao.deleteById(playerUUID.toString());
             return true;
         } catch (SQLException e) {
@@ -99,7 +163,7 @@ public class AttendanceDbContext {
     public Optional<UserAttendance> fetchUser(UUID playerUUID) {
         try {
             var user = userAttendanceDao.queryForId(playerUUID.toString());
-            return Optional.of(user);
+            return Optional.ofNullable(user);
         } catch (SQLException e) {
             LOGGER.error("Failed to fetch user", e);
             return Optional.empty();
@@ -113,5 +177,23 @@ public class AttendanceDbContext {
             LOGGER.error("Failed to fetch users", e);
         }
         return List.of();
+    }
+
+    public UserMonthlyAttendance fetchUserMonthly(String playerUUID,int month){
+        try {
+            var user = userAttendanceDao.queryForId(playerUUID);
+            if(user == null){
+                return null;
+            }
+            return userMonthlyAttendanceDao.queryBuilder()
+                    .where()
+                    .eq("userAttendance_id", user.getId())
+                    .and()
+                    .eq("month", month)
+                    .queryForFirst();
+        } catch (SQLException e) {
+            LOGGER.error("Failed to fetch user monthly", e);
+            return null;
+        }
     }
 }
